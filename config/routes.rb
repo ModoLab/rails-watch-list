1e87cc1c074f7572ace251c144e9637f51aaddae
edouard modolo <edouardtravers@gmail.com>
2023-11-17 12:38:07 +0100

    test1

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,28 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "lists#index"
+
+  resources :lists do
+      resources :bookmarks
+  end
+  resources :bookmarks
+end
+
+
+Rails.application.routes.draw do
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks
+
+  # Pour la ressource Movie, cela dépend de la manière dont vous souhaitez l'utiliser.
+  # Si Movie est une ressource indépendante, vous pouvez le définir comme suit :
+  resources :movies
+
+  # Si Movie est utilisé à travers les bookmarks et les lists, vous pouvez le définir de manière imbriquée :
+  resources :movies do
+    resources :bookmarks, only: [:new, :create]
+  end
 end
